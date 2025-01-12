c94bf9395c69df020772b615b214f924026c769b
Martin263-gif <antonovkiril63@gmail.com>
2025-01-12 11:26:44 +0200

    Coordinates

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,28 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Game : MonoBehaviour
+namespace daw
 {
-    public GameObject chesspiece;
-
-    private GameObject[,] positions = new GameObject[8, 8];
-    private GameObject[] playerBlack = new GameObject[16];
-    private GameObject[] playerWhite = new GameObject[16];
-
-    private string currentPlayer = "white";
-
-    private bool gameOver = false;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Game : MonoBehaviour
     {
-        playerWhite = new GameObject[]
-        {
-            Create("white_rook", -2.32, -2.3), Create("white_knight", -1.65, -2.3), Create("white_bishop", -0.98, -2.3), Create("white_queen", -0.31, -2.3), Create("white_king", 0.36, -2.3), Create("white_bishop", 1.03, -2.3), Create("white_knight", 1.7, -2.3), Create("white_rook",2.37,-2.3), Create("white_pawn", -2.32, -1.65), Create("white_pawn", -1.65, -1.65), Create("white_pawn", -0.98, -1.65), Create("white_pawn", -0.31, -1.65), Create("white_pawn", 0.36, -1.65), Create("white_pawn", 1.03, -1.65), Create("white_pawn", 1.7, -1.65), Create("white_pawn",2.37,-1.65),
-        }
-    }
+        public GameObject chesspiece;
 
-    // Update is called once per frame
-    
+        private GameObject[,] positions = new GameObject[8, 8];
+        private GameObject[] playerBlack = new GameObject[16];
+        private GameObject[] playerWhite = new GameObject[16];
+
+        private string currentPlayer = "white";
+
+        private bool gameOver = false;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerWhite = new GameObject[]
+            {
+            Create("white_rook", 0, 0), Create("white_knight", 1, 0), Create("white_bishop", 2, 0), Create("white_queen", 3, 0), Create("white_king", 4, 0), Create("white_bishop", 5, 0), Create("white_knight",    6, 0), Create("white_rook",7, 0), Create("white_pawn", 1, 1), Create("white_pawn", 2, 1), Create("white_pawn", 3, 1), Create("white_pawn", 4, 1), Create("white_pawn", 0, 1), Create("white_pawn", 5, 1), Create("white_pawn", 6, 1), Create("white_pawn",7,1) };
+
+
+            playerBlack = new GameObject[]
+            {
+                Create("black_rook", 0, 7), Create("black_knight", 1, 7), Create("black_bishop", 2, 7), Create("black_queen", 3, 7), Create("black_king", 4, 7), Create("black_bishop", 5, 7), Create("black_knight",    6, 7), Create("black_rook",7, 7), Create("black_pawn", 1, 6), Create("white_pawn", 2, 6), Create("white_pawn", 3, 6), Create("white_pawn", 4, 6), Create("white_pawn", 0, 6), Create("white_pawn", 5, 6), Create("white_pawn", 6, 6), Create("white_pawn",7,6) 
+            };
+
+            for (int i = 0; i < playerBlack.Length; i++)
+            {
+                SetPosition(playerBlack[i]);
+                SetPosition(playerWhite[i]);
+            } 
+            
+
+
+
+        }
+
+        public GameObject Create(string name, int x, int y)
+        {
+            GameObject obj = Instantiate(chesspiece, new Vector3(0,0,-1), Quaternion.identity);
+            ChessMan cm = obj.GetComponent<ChessMan>();
+            cm.name = name;
+            cm.SetXBoard(x);
+            cm.SetXBoard(y);
+            cm.Activate();
+            return obj;
+        }
+
+        public void SetPosition(GameObject obj)
+        {
+            ChessMan cm = obj.GetComponent<ChessMan>();
+            positions[cm.GetXBoard(), cm.GetYBoard()] = obj;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Update is called once per frame
+
+    }
 }
