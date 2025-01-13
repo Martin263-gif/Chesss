dbec1ee1a2ec1458e86511cbc6c01412dcaa6de4
Martin263-gif <antonovkiril63@gmail.com>
2025-01-13 17:48:29 +0200

    Moving Attacking

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -47,7 +47,7 @@ namespace daw
             ChessMan cm = obj.GetComponent<ChessMan>();
             cm.name = name;
             cm.SetXBoard(x);
-            cm.SetXBoard(y);
+            cm.SetYBoard(y);
             cm.Activate();
             return obj;
         }
@@ -56,6 +56,22 @@ namespace daw
         {
             ChessMan cm = obj.GetComponent<ChessMan>();
             positions[cm.GetXBoard(), cm.GetYBoard()] = obj;
+        }
+
+        public void SetPositionEmpty(int x, int y)
+        {
+            positions[x, y] = null;
+        }
+
+        public GameObject GetPosition(int x, int y)
+        {
+            return positions[x, y];
+        }
+
+        public bool PositionOnBoard(int x, int y)
+        {
+            if(x< 0 || y<0 || x >= positions.GetLength(0) || y >= positions.GetLength(1)) return false;
+            return true;
         }
 
 
---
+++ Assets/Scripts/MovePlate.cs
@@ -0,0 +1,66 @@
+using daw;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlate : MonoBehaviour
+{
+    public GameObject controller;
+
+
+    GameObject reference = null;
+
+    int matrixX;
+    int matrixY;
+
+    public bool attack = false;
+
+
+    public void Start()
+    {
+        if(attack)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+        }
+    }
+
+    public void OnMouseUp()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+
+        if(attack )
+        {
+            GameObject cp = controller.GetComponent<Game>().GetPosition(matrixX, matrixY);
+
+            Destroy(cp);
+        }
+
+        controller.GetComponent<Game>().SetPositionEmpty(reference.GetComponent<ChessMan>().GetXBoard(),
+            reference.GetComponent<ChessMan>().GetYBoard());
+
+        reference.GetComponent<ChessMan>().SetXBoard(matrixX);
+        reference.GetComponent<ChessMan>().SetYBoard(matrixY);
+        reference.GetComponent<ChessMan>().SetCords();
+
+
+        controller.GetComponent<Game>().SetPosition(reference);
+
+        reference.GetComponent<ChessMan>().DestroyMovePlates();
+
+    }
+
+    public void SetCords(int x, int y)
+    {
+        matrixX = x;
+        matrixY = y;
+    }
+    public void SetReference(GameObject obj)
+    {
+        reference = obj;
+    }
+
+    public GameObject GetReference()
+    {
+        return reference;   
+    }
+}
